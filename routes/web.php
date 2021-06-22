fa38b43759b82b5b5e3a40c1a65869045ec4265f
Damas Amirul Karim <paydamas@gmail.com>
2021-06-22 20:48:39 +0700

    install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome')->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
