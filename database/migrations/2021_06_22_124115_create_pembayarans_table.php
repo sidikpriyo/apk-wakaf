0fa00fa392e768057a78ee3cc7668972cf19fd9b
Damas Amirul Karim <paydamas@gmail.com>
2021-06-22 19:43:42 +0700

    menambahkan tabel pembayaran

---
+++ database/migrations/2021_06_22_124115_create_pembayarans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePembayaransTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 50);
+            $table->string('kode', 10);
+            $table->unsignedBigInteger('jenis_pembayaran_id');
+            $table->timestamps();
+
+            $table->foreign('jenis_pembayaran_id')->references('id')->on('jenis_pembayarans')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+}
